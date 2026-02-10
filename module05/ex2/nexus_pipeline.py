d2aaf8dfeefdf4f7cafc07c59b23922e213e95e1
the8-swan <hananebmd077@gmail.com>
2026-02-10 03:24:32 +0100

    ex2 processing ..

--- module05/ex2/nexus_pipeline.py
+++ module05/ex2/nexus_pipeline.py
@@ -10,12 +10,18 @@ class ProcessingPipeline(ABC):
 	def __init__(self):
 		self.stages = []
 	
-	def add_stage(self):
+	def add_stage(self, stage: ProcessingStage):
+		self.stages.append(stage)
+
+	@abstractmethod
+	def process(self, data: Any)-> Any:
 		pass
+
 
 class InputStage:
 	def process(self, data: Any)-> Any:
-		pass
+		print(f"Input: {data}")
+		return data
 
 class TransformStage:
 	def process(self, data: Any)-> Any:
@@ -27,24 +33,28 @@ class OutputStage:
 
 class JSONAdapter(ProcessingPipeline):
 	def __init__(self, pipeline_id: int):
+		super().__init__()
 		self.pipeline_id = pipeline_id
 
 	def process(self, data: Any)-> Any:
-		pass
+		self.stages[0].process(data)
 
-class CSVAdapter(ProcessingPipeline):
-	def __init__(self, pipeline_id: int):
-		self.pipeline_id = pipeline_id
 
-	def process(self, data: Any)-> Any:
-		pass
+# class CSVAdapter(ProcessingPipeline):
+# 	def __init__(self, pipeline_id: int):
+# 		super().__init__()
+# 		self.pipeline_id = pipeline_id
 
-class StreamAdapter(ProcessingPipeline):
-	def __init__(self, pipeline_id: int):
-		self.pipeline_id = pipeline_id
+# 	def process(self, data: Any)-> Any:
+# 		pass
 
-	def process(self, data: Any)-> Any:
-		pass
+# class StreamAdapter(ProcessingPipeline):
+# 	def __init__(self, pipeline_id: int):
+# 		super().__init__()
+# 		self.pipeline_id = pipeline_id
+
+# 	def process(self, data: Any)-> Any:
+# 		pass
 
 
 class NexusManager():
@@ -54,7 +64,32 @@ class NexusManager():
 
 def main()-> None:
 	print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===\n")
+	json_data = {"sensor": "temp", "value": 23.5, "unit": "C"}
 	nexusManager = NexusManager()
+	print("Creating Data Processing Pipeline...\n"
+		  "Stage 1: Input validation and parsing\n"
+		  "Stage 2: Data transformation and enrichment\n"
+		  "Stage 3: Output formatting and delivery\n")
+
+	print("=== Multi-Format Data Processing ===\n")
+
+	jsonAdapter = JSONAdapter(1)
+	jsonAdapter.add_stage(InputStage())
+	jsonAdapter.add_stage(TransformStage())
+	jsonAdapter.add_stage(OutputStage())
+
+	print("Processing JSON data through pipeline...")
+	jsonAdapter.process(json_data)
+
+	# csvAdapter = CSVAdapter(1)
+	# csvAdapter.add_stage(InputStage)
+	# csvAdapter.add_stage(TransformStage)
+	# csvAdapter.add_stage(OutputStage)
+
+	# streamAdapter = StreamAdapter(1)
+	# streamAdapter.add_stage(InputStage)
+	# streamAdapter.add_stage(TransformStage)
+	# streamAdapter.add_stage(OutputStage)
 
 
 main()
